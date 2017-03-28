e79dbd5312cda0a6509e6be0f03135701cb86fd9
John Cordeiro <john.dalton@ilhasoft.com.br>
2017-03-28 12:20:03 -0300

    Create default config file with sample data

---
+++ app/src/main/java/io/rapidpro/androidchannel/Config.java
@@ -0,0 +1,13 @@
+package io.rapidpro.androidchannel;
+
+public class Config {
+    //This is the URL of your RapidPRO instance
+    public static final String ENDPOINT = "https://your-rapidpro-endpoint.com";
+
+    //These settings are from Google Cloud Messaging. Check here: https://developers.google.com/cloud-messaging/
+    public static final String GCM_API_KEY = "GCM Api Key";
+    public static final String GCM_APP_ID = "GCM App ID";
+    
+    //RapidPRO name, in the case that you have a different branding
+    public static final String RAPIDPRO_NAME = "RapidPRO Name";
+}
